f19bac4565232954687c23176f494b99fc3bcf50
DanielAArif <daniel.arif@mhs.unsoed.ac.id>
2024-11-04 10:33:03 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qumon/ui/login_page.dart';
+import 'package:qumon/ui/registrasi_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
